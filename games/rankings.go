e85a5b9e20f08fe5a9ca50eda074ab4f2caba0eb
Sean Patrick Hagen <sean.hagen@gmail.com>
2021-11-15 19:24:40 -0800

    Fix some constructor issues, moving old code into here

--- games/rankings.go
+++ games/rankings.go
@@ -1,23 +1,131 @@
 package games
 
+import (
+	"strconv"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
 /**
  * File: rankings.go
  * Date: 2021-11-15 15:36:26
  * Creator: Sean Patrick Hagen <sean.hagen@gmail.com>
  */
 
-// Rankings ...
-type Rankings struct {
-	Teams      map[string]Team
-	Days       map[int]MatchDay
-	CurrentDay int
+// StartMatchDay is the day number that matches start on
+const StartMatchDay = 1
+
+// Ranking ...
+type Ranking struct {
+	Teams        map[string]*team
+	Days         map[int]matchDay
+	currentMatch *matchDay
 }
 
-// CreateRankings ...
-func CreateRankings() Rankings {
-	return Rankings{
-		Teams:      map[string]Team{},
-		Days:       map[int]MatchDay{},
-		CurrentDay: 1,
+// NewRanking ...
+func NewRanking() *Ranking {
+	r := Ranking{
+		Teams: map[string]*team{},
+		Days:  map[int]matchDay{},
 	}
+	cm := newMatchDay(&r, StartMatchDay)
+	r.Days[StartMatchDay] = cm
+	r.currentMatch = &cm
+	return &r
+}
+
+// newMatchDay ...
+func (r Ranking) newMatchDay() {
+	if r.currentMatch == nil {
+		cm := newMatchDay(&r, StartMatchDay)
+		r.Days[StartMatchDay] = cm
+		r.currentMatch = &cm
+		spew.Dump(cm)
+		return
+	}
+
+	nd := r.currentMatch.Day + 1
+	nm := newMatchDay(&r, nd)
+	spew.Dump(nm)
+	r.Days[nd] = nm
+	r.currentMatch = &nm
+}
+
+// currentDay ...
+func (r Ranking) currentDay() int {
+	return r.currentMatch.Day
+}
+
+// getTeam ...
+func (r Ranking) getTeam(n string) *team {
+	for k, v := range r.Teams {
+		if k == n {
+			return v
+		}
+	}
+	return nil
+}
+
+// AddMatch ...
+func (r Ranking) AddMatch(in string) error {
+	err := r.parseMatchLine(in)
+	if err != nil {
+		if _, ok := err.(*TeamPlayedError); ok {
+			r.newMatchDay()
+			return r.AddMatch(in)
+		}
+		return err
+	}
+	return nil
+}
+
+// parseMatchLine ...
+func (r Ranking) parseMatchLine(input string) error {
+	parts := strings.FieldsFunc(input, func(r rune) bool { return r == ',' })
+	if len(parts) != 2 {
+		return &ParseLineError{input}
+	}
+
+	t1, err := r.parseTeamScore(parts[0])
+	if err != nil {
+		return err
+	}
+
+	t2, err := r.parseTeamScore(parts[1])
+	if err != nil {
+		return err
+	}
+
+	err = r.currentMatch.processMatchResults(t1, t2)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// teamResult ...
+type teamResult struct {
+	team  *team
+	score int
+}
+
+// parseTeamScore ...
+func (r Ranking) parseTeamScore(in string) (*teamResult, error) {
+	in = strings.TrimSpace(in)
+	bits := strings.Fields(in)
+	x := len(bits)
+	if x <= 0 {
+		return nil, &ParseTeamError{empty: true}
+	}
+
+	name := strings.Join(bits[0:x-1], " ")
+	score, err := strconv.Atoi(string(bits[x-1]))
+	if err != nil {
+		return nil, &ParseTeamError{score: string(bits[x-1]), err: err}
+	}
+
+	t := r.findOrCreateTeam(name)
+	return &teamResult{team: t, score: score}, nil
 }
--- games/rankings_test.go
+++ games/rankings_test.go
@@ -1,7 +1,101 @@
 package games
 
+import (
+	"fmt"
+	"testing"
+)
+
 /**
  * File: rankings_test.go
  * Date: 2021-11-15 16:14:37
  * Creator: Sean Patrick Hagen <sean.hagen@gmail.com>
  */
+
+func TestGames_Ranking_ParseMatchLine(t *testing.T) {
+	tests := []struct {
+		line   string
+		t1, t2 string
+		s1, s2 int
+		ok     bool
+	}{
+		{"San Jose Earthquakes 3, Santa Cruz Slugs 3", "San Jose Earthquakes", "Santa Cruz Slugs", 3, 3, true},
+		{"Vancouver Bears 1, Calgary Lions 0", "Vancouver Bears", "Calgary Lions", 1, 0, true},
+		{"A 2, B 3", "A", "B", 2, 3, true},
+		{"A 22, B 35", "A", "B", 22, 35, true},
+		{"      A 2,           B 3", "A", "B", 2, 3, true},
+		{"A B C D 5,D E F G H 4", "A B C D", "D E F G H", 5, 4, true},
+		{"A B C D 5,       D E F G H 4", "A B C D", "D E F G H", 5, 4, true},
+		{"A A A A A A 1, ", "A", "", 1, -1, false},
+		{",B B B 2", "", "B", -1, -1, false},
+		{"A, 2, B, 3", "A", "B", -1, -1, false},
+		{"A, 2 B 3", "A", "B", -1, -1, false},
+		{"A 2 B 3", "A", "B", -1, -1, false},
+		{"", "", "", -1, -1, false},
+	}
+
+	for i, x := range tests {
+		tt := x
+		t.Run(fmt.Sprintf("test_%v_", i), func(t *testing.T) {
+			r := NewRanking()
+			err := r.parseMatchLine(tt.line)
+			if tt.ok && err != nil {
+				t.Fatalf("expected okay, got error: %v", err)
+			}
+
+			if tt.ok && err == nil {
+				t1 := r.getTeam(tt.t1)
+				if t1 == nil {
+					t.Fatalf("team '%v' is not present in rankings", tt.t1)
+				}
+
+				s1 := r.currentMatch.Teams[tt.t1]
+				if s1 != tt.s1 {
+					t.Errorf("Wrong score for team '%v', expected '%v' got '%v'", tt.t1, tt.s1, s1)
+				}
+
+				t2 := r.getTeam(tt.t2)
+				if t2 == nil {
+					t.Fatalf("team '%v' is not present in rankings", tt.t2)
+				}
+
+				s2 := r.currentMatch.Teams[tt.t2]
+				if s2 != tt.s2 {
+					t.Errorf("Wrong score for team '%v', expected '%v' got '%v'", tt.t2, tt.s2, s2)
+				}
+
+				o1 := r.currentMatch.Matchups[tt.t1]
+				if o1 != tt.t2 {
+					t.Errorf("Wrong opponent recorded for team '%v', expected '%v' got '%v'", tt.t1, tt.t2, o1)
+				}
+
+				o2 := r.currentMatch.Matchups[tt.t2]
+				if o2 != tt.t1 {
+					t.Errorf("Wrong opponent recorded for team '%v', expected '%v' got '%v'", tt.t2, tt.t1, o2)
+				}
+
+				if s1 > s2 {
+					if t1.Rank != 3 {
+						t.Errorf("expected rank for '%v' after win to be 3, rank is: %v", t1.Name, t1.Rank)
+					}
+					if t2.Rank != 0 {
+						t.Errorf("expected rank for '%v' after loss to be 0, rank is: %v", t2.Name, t2.Rank)
+					}
+				} else if s2 > s1 {
+					if t1.Rank != 0 {
+						t.Errorf("expected rank for '%v' after loss to be 0, rank is: %v", t1.Name, t1.Rank)
+					}
+					if t2.Rank != 3 {
+						t.Errorf("expected rank for '%v' after win to be 3, rank is: %v", t2.Name, t2.Rank)
+					}
+				} else {
+					if t1.Rank != 1 {
+						t.Errorf("expected rank for '%v' after tie to be 1, rank is: %v", t1.Name, t1.Rank)
+					}
+					if t2.Rank != 1 {
+						t.Errorf("expected rank for '%v' after tie to be 1, rank is: %v", t2.Name, t2.Rank)
+					}
+				}
+			}
+		})
+	}
+}
