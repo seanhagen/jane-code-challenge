7121942361ae91b95e6f8470b4656c8de5b166d4
Sean Patrick Hagen <sean.hagen@gmail.com>
2021-11-22 15:32:29 -0800

    Remove TODO comment

--- magefile.go
+++ magefile.go
@@ -457,7 +457,6 @@ func buildForAllPlatforms() error {
 
 func buildForCurrent(debug bool) error {
 	mg.Deps(initVars)
-	// TODO: run go build, output to output directory
 
 	env := getEnv()
 	args := []string{
